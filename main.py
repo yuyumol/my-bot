15581c9a07fcd9273f29ad5692a95b5925ccb3a9
yuyumol <yu14matsu@gmail.com>
2026-09-15 11:22:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("GitHub Actions から Python が動きました")
+import os
+import requests
+
+url = os.environ["DISCORD_WEBHOOK_URL"]
+
+response = requests.post(
+    url,
+    json={
+        "username": "dailyreport_bot",
+        "content": "🧪 dailyreport_bot 接続テスト成功！"
+    },
+    timeout=30
+)
+
+response.raise_for_status()
+print("Discordへの送信に成功しました")
